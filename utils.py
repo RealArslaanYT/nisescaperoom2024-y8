02620ddee64f41e55df0ce3d4614f5d6c72900af
Arslaan <118288807+RealArslaanYT@users.noreply.github.com>
2024-05-29 18:00:13 +1200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 from functools import wraps
-from flask import make_response, request, current_app
+from flask import make_response, request
 
 def auth_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         auth = request.authorization
-        if auth and auth.username == current_app.config["SITE_USER"] and auth.password == current_app.config["SITE_PASS"]:
+        if auth and auth.username == "CHEFGALAESCAPE" and auth.password == "identifying-angles":
             return f(*args, **kwargs)
         return make_response("<h1>Access denied!</h1>", 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
 
