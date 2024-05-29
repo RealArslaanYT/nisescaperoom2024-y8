fd22bd12e11a88d0bfc92b687734e0e4f86df744
Arslaan <118288807+RealArslaanYT@users.noreply.github.com>
2024-05-29 17:54:30 +1200

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,12 @@ from .utils import auth_required
 
 def create_app():
     app = Flask(__name__)
-
     app.config.from_prefixed_env()
 
     @app.route("/")
     @auth_required
     def index():
-        return "<h1>Hello, World!</h1>"
-
-    @app.route("/home")
-    @auth_required
-    def home():
-        return "<h1>Home!</h1>"
+        return render_template("index.html")
 
     return app
 
